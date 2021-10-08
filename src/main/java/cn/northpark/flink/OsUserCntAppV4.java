d0f07b346f51a9340b4414644fdd6e9d7584aa35
liuhouer <654714226@qq.com>
2021-10-08 14:25:21 +0800

    利用布隆过滤器来实现超级大内容的缓存判定，比查询数据库高效的多

---
+++ src/main/java/cn/northpark/flink/OsUserCntAppV4.java
@@ -0,0 +1,90 @@
+package cn.northpark.flink;
+
+import cn.northpark.flink.bean.Access;
+import com.alibaba.fastjson.JSON;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.shaded.guava18.com.google.common.hash.BloomFilter;
+import org.apache.flink.shaded.guava18.com.google.common.hash.Funnels;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+
+/**
+ * 新老用户的统计分析
+ * 原来是根据数据中的某个字段
+ * 现在我们是根据每个device来进行判断是否是新老用户
+ * 思考：device放到state里面去呢
+ * 我们的实现：状态 + 布隆过滤器
+ * 【注意：如果是日志中的某个字段有具体业务含义，判断+更新时需要用别的字段，不然没有必要实现】
+ */
+public class OsUserCntAppV4 {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        SingleOutputStreamOperator<Access> cleanStream = environment.socketTextStream("localhost", 9527)
+                .map(new MapFunction<String, Access>() {
+                    @Override
+                    public Access map(String value) throws Exception {
+                        // TODO...  json ==> Access
+
+                        try {
+                            return JSON.parseObject(value, Access.class);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+
+                    }
+                }).filter(x -> x != null)
+                .filter(new FilterFunction<Access>() {
+                    @Override
+                    public boolean filter(Access value) throws Exception {
+                        return "startup".equals(value.event);
+                    }
+                });
+
+
+        cleanStream.keyBy(x -> x.deviceType)
+                .process(new KeyedProcessFunction<String, Access, Access>() {
+
+                    private transient ValueState<BloomFilter<String>> state;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        ValueStateDescriptor<BloomFilter<String>> descriptor = new ValueStateDescriptor<>("s", TypeInformation.of(new TypeHint<BloomFilter<String>>() {}));
+                        state = getRuntimeContext().getState(descriptor);
+                    }
+
+                    @Override
+                    //利用布隆过滤器来实现超级大内容的缓存判定，比查询数据库高效的多
+                    public void processElement(Access value, Context ctx, Collector<Access> out) throws Exception {
+                        String device = value.device;
+                        BloomFilter<String> bloomFilter = state.value();
+                        if(null == bloomFilter) {
+                            bloomFilter = BloomFilter.create(Funnels.unencodedCharsFunnel(), 100000);
+                        }
+
+                        if(!bloomFilter.mightContain(device)) {
+                            bloomFilter.put(device);
+                            value.nu2 = 1;
+                            state.update(bloomFilter);
+                        }
+
+                        out.collect(value);
+                    }
+                }).print();
+
+
+        environment.execute("OsUserCntAppV2");
+
+    }
+}

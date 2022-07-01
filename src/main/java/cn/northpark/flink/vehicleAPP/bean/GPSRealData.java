94fb3fa4057eeced10d0a44330cced387f9e47dd
liuhouer <654714226@qq.com>
2022-07-01 17:59:50 +0800

    车辆信息统计和实时处理

---
+++ src/main/java/cn/northpark/flink/vehicleAPP/bean/GPSRealData.java
@@ -0,0 +1,352 @@
+
+package cn.northpark.flink.vehicleAPP.bean;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
+public class GPSRealData {
+
+    @Expose
+    private String alarmState;
+    @Expose
+    private Double altitude;
+    @Expose
+    private Long areaAlarm;
+    @Expose
+    private Long areaId;
+    @Expose
+    private Long areaType;
+    @Expose
+    private Long depId;
+    @Expose
+    private Long direction;
+    @Expose
+    private String dvrStatus;
+    @Expose
+    private Double gas;
+    @Expose
+    private Long id;
+    @Expose
+    private Double latitude;
+    @Expose
+    private String location;
+    @Expose
+    private Double longitude;
+    @Expose
+    private Double mileage;
+    @Expose
+    private Boolean online;
+    @Expose
+    private Long onlineDate;
+    @Expose
+    private Long overSpeedAreaId;
+    @Expose
+    private Long overSpeedAreaType;
+    @Expose
+    private Long parkingTime;
+    @Expose
+    private String plateNo;
+    @Expose
+    private Double recordVelocity;
+    @Expose
+    private Long responseSn;
+    @Expose
+    private Long routeAlarmType;
+    @Expose
+    private Long routeSegmentId;
+    @Expose
+    private Long runTimeOnRoute;
+    @Expose
+    private Long sendTime;
+    @Expose
+    private Long signalState;
+    @Expose
+    private String simNo;
+    @Expose
+    private String status;
+    @Expose
+    private Long tiredAlarmTime;
+    @Expose
+    private Long updateDate;
+    @Expose
+    private Boolean valid;
+    @Expose
+    private Long vehicleId;
+    @Expose
+    private Double velocity;
+
+    public String getAlarmState() {
+        return alarmState;
+    }
+
+    public void setAlarmState(String alarmState) {
+        this.alarmState = alarmState;
+    }
+
+    public Double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(Double altitude) {
+        this.altitude = altitude;
+    }
+
+    public Long getAreaAlarm() {
+        return areaAlarm;
+    }
+
+    public void setAreaAlarm(Long areaAlarm) {
+        this.areaAlarm = areaAlarm;
+    }
+
+    public Long getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Long areaId) {
+        this.areaId = areaId;
+    }
+
+    public Long getAreaType() {
+        return areaType;
+    }
+
+    public void setAreaType(Long areaType) {
+        this.areaType = areaType;
+    }
+
+    public Long getDepId() {
+        return depId;
+    }
+
+    public void setDepId(Long depId) {
+        this.depId = depId;
+    }
+
+    public Long getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Long direction) {
+        this.direction = direction;
+    }
+
+    public String getDvrStatus() {
+        return dvrStatus;
+    }
+
+    public void setDvrStatus(String dvrStatus) {
+        this.dvrStatus = dvrStatus;
+    }
+
+    public Double getGas() {
+        return gas;
+    }
+
+    public void setGas(Double gas) {
+        this.gas = gas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Double mileage) {
+        this.mileage = mileage;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
+    public Long getOnlineDate() {
+        return onlineDate;
+    }
+
+    public void setOnlineDate(Long onlineDate) {
+        this.onlineDate = onlineDate;
+    }
+
+    public Long getOverSpeedAreaId() {
+        return overSpeedAreaId;
+    }
+
+    public void setOverSpeedAreaId(Long overSpeedAreaId) {
+        this.overSpeedAreaId = overSpeedAreaId;
+    }
+
+    public Long getOverSpeedAreaType() {
+        return overSpeedAreaType;
+    }
+
+    public void setOverSpeedAreaType(Long overSpeedAreaType) {
+        this.overSpeedAreaType = overSpeedAreaType;
+    }
+
+    public Long getParkingTime() {
+        return parkingTime;
+    }
+
+    public void setParkingTime(Long parkingTime) {
+        this.parkingTime = parkingTime;
+    }
+
+    public String getPlateNo() {
+        return plateNo;
+    }
+
+    public void setPlateNo(String plateNo) {
+        this.plateNo = plateNo;
+    }
+
+    public Double getRecordVelocity() {
+        return recordVelocity;
+    }
+
+    public void setRecordVelocity(Double recordVelocity) {
+        this.recordVelocity = recordVelocity;
+    }
+
+    public Long getResponseSn() {
+        return responseSn;
+    }
+
+    public void setResponseSn(Long responseSn) {
+        this.responseSn = responseSn;
+    }
+
+    public Long getRouteAlarmType() {
+        return routeAlarmType;
+    }
+
+    public void setRouteAlarmType(Long routeAlarmType) {
+        this.routeAlarmType = routeAlarmType;
+    }
+
+    public Long getRouteSegmentId() {
+        return routeSegmentId;
+    }
+
+    public void setRouteSegmentId(Long routeSegmentId) {
+        this.routeSegmentId = routeSegmentId;
+    }
+
+    public Long getRunTimeOnRoute() {
+        return runTimeOnRoute;
+    }
+
+    public void setRunTimeOnRoute(Long runTimeOnRoute) {
+        this.runTimeOnRoute = runTimeOnRoute;
+    }
+
+    public Long getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(Long sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public Long getSignalState() {
+        return signalState;
+    }
+
+    public void setSignalState(Long signalState) {
+        this.signalState = signalState;
+    }
+
+    public String getSimNo() {
+        return simNo;
+    }
+
+    public void setSimNo(String simNo) {
+        this.simNo = simNo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getTiredAlarmTime() {
+        return tiredAlarmTime;
+    }
+
+    public void setTiredAlarmTime(Long tiredAlarmTime) {
+        this.tiredAlarmTime = tiredAlarmTime;
+    }
+
+    public Long getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Long updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(Boolean valid) {
+        this.valid = valid;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public Double getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Double velocity) {
+        this.velocity = velocity;
+    }
+
+}

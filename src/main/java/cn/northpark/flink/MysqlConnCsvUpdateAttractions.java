2ad6ba2e11375b220850e043e9ab0783f9e27f67
liuhouer <654714226@qq.com>
2023-03-15 21:19:19 +0800

    mysql 关联 csv的数据集，更新mysql表的部分字段数据

--- src/main/java/cn/northpark/flink/MysqlConnCsvUpdateAttractions.java
+++ src/main/java/cn/northpark/flink/MysqlConnCsvUpdateAttractions.java
@@ -1,21 +1,20 @@
 package cn.northpark.flink;
 
-import org.apache.flink.table.descriptors.Csv;
-import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.jdbc.JDBCInputFormat;
 import org.apache.flink.api.java.io.jdbc.JDBCOutputFormat;
+import org.apache.flink.api.java.io.jdbc.JDBCOutputFormat.JDBCOutputFormatBuilder;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
-import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.TableSchema;
-import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
-import org.apache.flink.table.descriptors.FileSystem;
-import org.apache.flink.table.descriptors.Schema;
-import org.apache.flink.table.functions.ScalarFunction;
 import org.apache.flink.types.Row;
-import org.apache.flink.util.Collector;
+
+import java.util.Properties;
 
 /**
  * @author bruce
@@ -25,124 +24,93 @@ import org.apache.flink.util.Collector;
  */
 public class MysqlConnCsvUpdateAttractions {
 
-    public static void main(String[] args) throws Exception{
-        // Set up the Flink batch environment
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+    public static void main(String[] args) throws Exception {
 
-        // Set up the JDBC input format for the attractions table
-        String attractionsQuery = "SELECT name, region, category, address, comment, price, sales, province, longitude, latitude FROM t_attractions";
+        // 设置 MySQL 数据库连接属性
+        Properties properties = new Properties();
+        properties.setProperty("driverClassName", "com.mysql.jdbc.Driver");
+        properties.setProperty("jdbcUrl", "jdbc:mysql://localhost:3306/trip");
+        properties.setProperty("username", "root");
+        properties.setProperty("password", "123456");
+
+        // 创建 MySQL 输入格式
         JDBCInputFormat inputFormat = JDBCInputFormat.buildJDBCInputFormat()
                 .setDrivername("com.mysql.jdbc.Driver")
-                .setDBUrl("jdbc:mysql://localhost:3306/mydb")
-                .setUsername("myuser")
-                .setPassword("mypassword")
-                .setQuery(attractionsQuery)
-                .setRowTypeInfo(new RowTypeInfo(
-                        Types.STRING, // name
-                        Types.STRING, // region
-                        Types.STRING, // category
-                        Types.STRING, // address
-                        Types.STRING, // comment
-                        Types.DOUBLE, // price
-                        Types.INT,    // sales
-                        Types.STRING, // province
-                        Types.DOUBLE, // longitude
-                        Types.DOUBLE  // latitude
-                ))
+                .setDBUrl("jdbc:mysql://localhost:3306/trip")
+                .setUsername("root")
+                .setPassword("123456")
+                .setQuery("SELECT name, region, category, longitude, latitude FROM t_attractions")
+                .setRowTypeInfo(new RowTypeInfo(org.apache.flink.api.common.typeinfo.Types.STRING,
+                        org.apache.flink.api.common.typeinfo.Types.STRING,
+                        org.apache.flink.api.common.typeinfo.Types.STRING,
+                        org.apache.flink.api.common.typeinfo.Types.DOUBLE,
+                        org.apache.flink.api.common.typeinfo.Types.DOUBLE))
                 .finish();
 
-        // Read the attractions table as a DataSet<Row>
-        DataSet<Row> attractions = env.createInput(inputFormat);
+        // 创建 Flink 批处理环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        // Set up the CSV file format for the Excel file
-        String filePath = "/path/to/excel/file.xlsx";
-        Csv csv = new Csv()
-                .fieldDelimiter('\t')
-//                .ignoreFirstLine(true)
-                .ignoreParseErrors()
-                .allowComments()
-                .schema(TableSchema.builder()
-                        .field("name", DataTypes.STRING())
-                        .field("region", DataTypes.STRING())
-                        .field("category", DataTypes.STRING())
-                        .field("longitude", DataTypes.DOUBLE())
-                        .field("latitude", DataTypes.DOUBLE())
-                        .build());
+        // 读取 MySQL 数据并转换成 Tuple5<String, String, String, Double, Double> 类型
+        DataSet<Tuple5<String, String, String, Double, Double>> attractions = env.createInput(inputFormat)
+                .map(new MapFunction<Row, Tuple5<String, String, String, Double, Double>>() {
+                    @Override
+                    public Tuple5<String, String, String, Double, Double> map(Row row) throws Exception {
+                        return Tuple5.of((String) row.getField(0), (String) row.getField(1), (String) row.getField(2),
+                                (Double) row.getField(3), (Double) row.getField(4));
+                    }
+                });
 
-        // Set up the Flink table environment
-        BatchTableEnvironment tEnv = BatchTableEnvironment.create(env);
+        // 读取 Excel 数据并转换成 Tuple3<String, String, Tuple2<Double, Double>> 类型
+        DataSet<Tuple3<String, String, Tuple2<Double, Double>>> excelData = env.readCsvFile("src/main/resources/A级景区经纬度.xlsx")
+                .types(String.class, String.class, Double.class, Double.class)
+                .map(new MapFunction<Tuple4<String, String, Double, Double>, Tuple3<String, String, Tuple2<Double, Double>>>() {
+                    @Override
+                    public Tuple3<String, String, Tuple2<Double, Double>> map(Tuple4<String, String, Double, Double> value) throws Exception {
+                        return Tuple3.of(value.f0, value.f1, Tuple2.of(value.f2, value.f3));
+                    }
+                });
 
-        // Register the attractions table as a Flink table
-        tEnv.createTemporaryView("t_attractions", attractions, "name, region, category, address, comment, price, sales, province, longitude, latitude");
+        // 关联 MySQL 数据和 Excel 数据
+        DataSet<Row> result = attractions.leftOuterJoin(excelData)
+                .where(0)
+                .equalTo(0)
+                .with(
+                        new JoinFunction<Tuple5<String, String, String, Double, Double>, Tuple3<String, String, Tuple2<Double, Double>>, Row>() {
+                            @Override
+                            public Row join(Tuple5<String, String, String, Double, Double> first, Tuple3<String, String, Tuple2<Double, Double>> second) throws Exception {
+                                Row row = new Row(5);
+                                row.setField(0, first.f0);
+                                row.setField(1, first.f1);
+                                row.setField(2, first.f2);
+                                row.setField(3, first.f3);
+                                row.setField(4, first.f4);
+                                if (second != null) {
+                                    row.setField(3, second.f2.f0);
+                                    row.setField(4, second.f2.f1);
+                                }
+                                return row;
+                            }
+                        });
 
-        // Register the Excel file as a Flink table
-        tEnv.connect(new FileSystem().path(filePath))
-                .withFormat(csv)
-                .withSchema(new Schema()
-                        .field("name", DataTypes.STRING())
-                        .field("region", DataTypes.STRING())
-                        .field("category", DataTypes.STRING())
-                        .field("longitude", DataTypes.DOUBLE())
-                        .field("latitude", DataTypes.DOUBLE())
-                )
-                .createTemporaryTable("t_excel");
 
-// Define a scalar function to update the attractions table
-        tEnv.registerFunction("updateAttractions", new UpdateAttractions());
-
-        // Join the attractions table with the Excel table
-        Table result = tEnv.sqlQuery(
-                "SELECT " +
-                        "a.name, a.region, a.category, a.address, a.comment, a.price, a.sales, a.province, " +
-                        "IF(e.longitude IS NULL, a.longitude, e.longitude) AS longitude, " +
-                        "IF(e.latitude IS NULL, a.latitude, e.latitude) AS latitude " +
-                        "FROM t_attractions a " +
-                        "LEFT JOIN t_excel e ON a.name = e.name"
-        );
-
-        // Convert the result to a DataSet<Row>
-        DataSet<Row> output = tEnv.toDataSet(result, Row.class);
-
-        // Set up the JDBC output format for the attractions table
-        JDBCOutputFormat outputFormat = JDBCOutputFormat.buildJDBCOutputFormat()
+        // 写入 MySQL 数据库
+        JDBCOutputFormatBuilder outputFormatBuilder = JDBCOutputFormat.buildJDBCOutputFormat()
                 .setDrivername("com.mysql.jdbc.Driver")
-                .setDBUrl("jdbc:mysql://localhost:3306/mydb")
-                .setUsername("myuser")
-                .setPassword("mypassword")
-                .setQuery("UPDATE t_attractions SET longitude=?, latitude=? WHERE name=?")
-                .setSqlTypes(new int[] { Types.DOUBLE, Types.DOUBLE, Types.STRING })
-                .finish();
+                .setDBUrl("jdbc:mysql://localhost:3306/trip")
+                .setUsername("root")
+                .setPassword("123456")
+                .setQuery("UPDATE t_attractions SET longitude = ?, latitude = ? WHERE name = ?")
+                .setSqlTypes(new int[]{java.sql.Types.DOUBLE, java.sql.Types.DOUBLE, java.sql.Types.VARCHAR});
 
-// Write the output to the attractions table
-        output.output(outputFormat);
+        result.output(JDBCOutputFormat.buildJDBCOutputFormat()
+                .setDrivername("com.mysql.jdbc.Driver")
+                .setDBUrl("jdbc:mysql://localhost:3306/trip")
+                .setUsername("root")
+                .setPassword("123456")
+                .setQuery("UPDATE t_attractions SET longitude = ?, latitude = ? WHERE name = ?")
+                .setSqlTypes(new int[]{java.sql.Types.DOUBLE, java.sql.Types.DOUBLE, java.sql.Types.VARCHAR})
+                .finish());
 
-// Execute the Flink job
-        env.execute("UpdateAttractions");
+        env.execute("Update Attractions");
     }
-
-
-    static class UpdateAttractions extends ScalarFunction {
-        public void eval(
-                String name,
-                String region,
-                String category,
-                String address,
-                String comment,
-                double price,
-                int sales,
-                String province,
-                double longitude,
-                double latitude,
-                double newLongitude,
-                double newLatitude,
-                Collector<Row> out
-        ) {
-            if (newLongitude != 0 && newLatitude != 0) {
-                out.collect(Row.of(name, region, category, address, comment, price, sales, province, newLongitude, newLatitude));
-            } else {
-                out.collect(Row.of(name, region, category, address, comment, price, sales, province, longitude, latitude));
-            }
-        }
-    }
-
 }
